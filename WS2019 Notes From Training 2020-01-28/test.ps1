db60108a10285d37b1a0ca1297d839ad1f6c8fbc
Mikael Nystrom <deploymentbunny@outlook.com>
2020-01-31 13:12:49 +0100

    Adding notes from new class

---
+++ WS2019 Notes From Training 2020-01-28/test.ps1
@@ -0,0 +1,12 @@
+Ladda hem Not My Fault
+Ladda hem WinDBG från 
+https://developer.microsoft.com/en-us/windows/downloads/windows-10-sdk
+Installera "Windows Debug Tools"
+
+Kör Not My Fault, välj ett valfritt sätt att ha sönder OS:et
+Logga in efter smällen, kör WinDBG x64, välj "Öppna dumpfilen", 
+efter att den är inläst, välj att köra !analyse -v
+
+Grattis, du har svaret i filen, det var "Not My Fault som troligen orsakade felet"
+
+https://www.networkworld.com/article/3100370/how-to-solve-windows-10-crashes-in-less-than-a-minute.html?page=8
